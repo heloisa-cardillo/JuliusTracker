8892b294331b9a5e06769746b3a98b7db985b64b
Heloisa Cardillo <heloisacardillo@gmail.com>
2023-04-19 18:10:53 -0300

    Initial commit

---
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.helo.juliustracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/MedicineFragment.kt
@@ -0,0 +1,6 @@
+package com.helo.juliustracker
+
+import androidx.fragment.app.Fragment
+
+class MedicineFragment : Fragment() {
+}
