cd34917d07dfbd446b138e515762fbdd14ec4f3c
Heloisa Cardillo <heloisacardillo@gmail.com>
2023-04-20 19:09:40 -0300

    Splashscreen Added

--- JuliusTracker/app/src/main/java/com/helo/juliustracker/MainActivity.kt
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.helo.juliustracker
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        window.statusBarColor= Color.parseColor("#FFFFFFFF")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+        val intent = Intent(this,ListDataFragment::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+
+
     }
 }
