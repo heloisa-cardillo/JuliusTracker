062db676685d3d9a07e38ce838bf95e73ed743ee
Heloisa Cardillo <heloisacardillo@gmail.com>
2023-07-18 01:44:16 -0300

    Toolbar

--- JuliusTracker/app/src/main/java/com/helo/juliustracker/FormFragment.kt
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/FormFragment.kt
@@ -3,14 +3,19 @@ package com.helo.juliustracker
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.graphics.drawable.Icon
 import android.media.MediaCodec.MetricsConstants.MODE
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,7 +25,7 @@ import com.google.android.material.textfield.TextInputLayout
 import com.helo.juliustracker.databinding.FragmentFormBinding
 
 class FormFragment : Fragment() {
-
+//    private lateinit var binding: FragmentFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +33,14 @@ class FormFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_form,container,false)
+
+//        binding.trash.setOnClickListener{
+//            Toast.makeText(requireContext(),"Clicou no buscar",Toast.LENGTH_LONG).show()
+//        }
+//
+//        binding.lupa.setOnClickListener{
+//            Toast.makeText(requireContext(),"Clicou no salvar",Toast.LENGTH_LONG).show()
+//        }
 
 
     }
@@ -52,13 +65,25 @@ class FormFragment : Fragment() {
         val sharedPref = Prefs(requireContext())
         sharedPref.name= "Heloisa"
 
+        val lupa_icon = view.findViewById<ImageView>(R.id.lupa)
+        lupa_icon.setOnClickListener{
+            Toast.makeText(requireContext(),"Clicou no buscar",Toast.LENGTH_SHORT).show()
+        }
+
+        val lixo_icon = view.findViewById<ImageView>(R.id.trash)
+        lixo_icon.setOnClickListener{
+            Toast.makeText(requireContext(),"Clicou no deletar",Toast.LENGTH_SHORT).show()
+        }
+
 
 
         val save_button = view.findViewById<Button>(R.id.save_button)
            save_button.setOnClickListener{
-               Toast.makeText(requireContext(),"Clicou no salvar",Toast.LENGTH_LONG).show()
+               Toast.makeText(requireContext(),"Clicou no salvar",Toast.LENGTH_SHORT).show()
 
         }
+
+
 
 
 //        view.findViewById<FloatingActionButton>(R.id.mais).setOnClickListener {
