af2364599b2f57c9bbb8474d1dad57870b710513
Heloisa Cardillo <heloisacardillo@gmail.com>
2023-08-09 18:53:50 -0300

    If it goes wrong again

--- JuliusTracker/app/src/main/java/com/helo/juliustracker/AdapterDetails.kt
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/AdapterDetails.kt
@@ -6,13 +6,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterDetails(private val itemsdetails: List<Medicine>): RecyclerView.Adapter<AdapterDetails.DetailsViewHolder>() {
+class AdapterDetails(private val itemsdetails: List<Medicine>) : RecyclerView.Adapter<AdapterDetails.DetailsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsViewHolder {
         //criar as visualizacoes em tela - criar os itens de lista
-        val viewDetails = LayoutInflater.from(parent.context).inflate(R.layout.fragment_form_item, parent, false)
-        val holder2 = DetailsViewHolder(viewDetails)
-        return holder2
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_form_item, parent, false)
+        return DetailsViewHolder(itemView)
+//        val holder2 = DetailsViewHolder(viewDetails)
+//        return holder2
     }
 
     override fun getItemCount(): Int = itemsdetails.size
@@ -20,12 +21,21 @@ class AdapterDetails(private val itemsdetails: List<Medicine>): RecyclerView.Ada
 
     override fun onBindViewHolder(holder2: DetailsViewHolder, position: Int) {
         //exibir as visualizacoes para o usuario
-        holder2.nomeremedio.text = itemsdetails[position].nomeremedio.toString()
-//        holder2.vezesaodia.text= itemsdetails[position].vezesaodia.toString()
+
+        val remedio = itemsdetails[position]
+        holder2.nomeremedio.text = remedio.toString()
+
+        val vezes = itemsdetails[position]
+        holder2.vezesaodia.text = vezes.toString()
+
+
+//        holder2.nomeremedio.text = itemsdetails[position].nomeremedio.toString()
+//        holder2.vezesaodia.text = itemsdetails[position].vezesaodia.toString()
     }
+
     class DetailsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nomeremedio = itemView.findViewById<TextView>(R.id.nome_remedio)
-//        val vezesaodia = itemView.findViewById<TextView>(R.id.vezes_dia)
+        val vezesaodia = itemView.findViewById<TextView>(R.id.vezes_dia)
     }
 
 }
--- JuliusTracker/app/src/main/java/com/helo/juliustracker/FormFragment.kt
+++ JuliusTracker/app/src/main/java/com/helo/juliustracker/FormFragment.kt
@@ -14,8 +14,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.Toast
 import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,7 +30,29 @@ import com.google.android.material.textfield.TextInputLayout
 import com.helo.juliustracker.databinding.FragmentFormBinding
 
 class FormFragment : Fragment() {
+    private lateinit var binding: FragmentFormBinding
+//
 //    private lateinit var binding: FragmentFormBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = FragmentFormBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        initRecyclerView1()
+//    }
+//
+//    private fun initRecyclerView1(){
+//        binding.rvRemedio.layoutManager = LinearLayoutManager(this)
+//        binding.rvRemedio.setHasFixedSize(true)
+//        binding.rvRemedio.adapter = AdapterDetails(getList())
+//    }
+//
+//    private fun getList() = listOf(
+//        "Gardenal",
+//        "Rivotril",
+//        "Brometo"
+//    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,16 +61,19 @@ class FormFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_form,container,false)
 
-//        binding.trash.setOnClickListener{
-//            Toast.makeText(requireContext(),"Clicou no buscar",Toast.LENGTH_LONG).show()
-//        }
-//
-//        binding.lupa.setOnClickListener{
-//            Toast.makeText(requireContext(),"Clicou no salvar",Toast.LENGTH_LONG).show()
-//        }
+        binding.trash.setOnClickListener{
+            Toast.makeText(requireContext(),"Clicou no buscar",Toast.LENGTH_LONG).show()
+        }
 
+        binding.lupa.setOnClickListener{
+            Toast.makeText(requireContext(),"Clicou no salvar",Toast.LENGTH_LONG).show()
+        }
+
+        binding = FragmentFormBinding.inflate(layoutInflater)
 
     }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -56,16 +85,18 @@ class FormFragment : Fragment() {
         rv_remedio.setHasFixedSize(true)
        val peso2 =  view.findViewById<TextInputLayout>(R.id.peso2)
         peso2.editText?.setText(formulario?.peso.toString()?:"meu ovo")
-
-
         val adapterDetails = AdapterDetails(listaMedice)
         //nao passa contexto para o adapter -> ja tem acesso e pode levar a problemas de memoria (memoryleak)
         rv_remedio.adapter= adapterDetails
 
 
-        val qtapapa = view.findViewById<TextInputLayout>(R.id.qtdpapa)
-        val sharedPref = Prefs(requireContext())
-        sharedPref.name= "Heloisa"
+
+
+
+
+//        val qtapapa = view.findViewById<TextInputLayout>(R.id.qtdpapa)
+//        val sharedPref = Prefs(requireContext())
+//        sharedPref.name= "Heloisa"
 
         val lupa_icon = view.findViewById<ImageView>(R.id.lupa)
         lupa_icon.setOnClickListener{
@@ -79,13 +110,14 @@ class FormFragment : Fragment() {
 
 
         view.findViewById<ExtendedFloatingActionButton>(R.id.save_button).setOnClickListener{
-            val formulario = Formulario(
-                data = "",
-                peso = 0.0,
-                cantou = false,
-                comeuracao = false,
-                remedios = emptyList()
-            )
+            findNavController().navigate(R.id.detail_to_medicine)
+//            val formulario = Formulario(
+//                data = "",
+//                peso = 0.0,
+//                cantou = false,
+//                comeuracao = false,
+//                remedios = emptyList()
+//            )
         }
         //findNavController().popBackStack() - FILO
 
@@ -95,7 +127,7 @@ class FormFragment : Fragment() {
 
 
 
-//        view.findViewById<FloatingActionButton>(R.id.mais).setOnClickListener {
+//        view.findViewById<FloatingActionButton>(R.id.save_button).setOnClickListener {
 //            findNavController().navigate(R.id.list_to_detail)
 //        }
 
